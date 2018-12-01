6d0f2716886f0a419ea598b396b88c60f1d2fe5e
Mausumi Ratha <Mausumi.Ratha@jda.com>
2018-12-01 23:13:40 +0530

    First Commit

---
+++ PTMTool/src/main/java/com/jda/PTMTool/PtmToolApplication.java
@@ -0,0 +1,12 @@
+package com.jda.PTMTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtmToolApplication.class, args);
+	}
+}
